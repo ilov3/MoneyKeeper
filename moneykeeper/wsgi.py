f62d982c82692a58a6e2de0826f506dd8d8c0f55
Bulat Kurbangaliev <iloveudead@gmail.com>
2015-11-17 20:15:07 +0300

    * minor fix

--- moneykeeper/wsgi.py
+++ moneykeeper/wsgi.py
@@ -13,7 +13,7 @@ from django.core.wsgi import get_wsgi_application
 from MoneyKeeper.utils.utils import setup_env
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
-base_dir = os.path.dirname(os.path.dirname(current_dir))
+base_dir = os.path.dirname(current_dir)
 sys.path.append(base_dir)
 setup_env(os.path.join(base_dir, 'envdir'))
 
