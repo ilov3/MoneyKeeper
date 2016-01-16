f2a5006fb8857e29aac9192e50f53d7fe43fadd0
ilov3 <iloveudead@gmail.com>
2016-01-16 14:24:16 +0300

    * favicon added

--- MoneyKeeper/urls.py
+++ MoneyKeeper/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url, include
 from django.views.generic import TemplateView
+from django.views.generic.base import RedirectView
 from rest_framework.routers import DefaultRouter
 from views import TransactionViewSet, AccountViewSet, CategoryViewSet, UserViewSet, ConfViewSet
 
@@ -12,6 +13,7 @@ router.register(r'user', viewset=UserViewSet, base_name='user')
 
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='app.html')),
+    url(r'^favicon\.ico$', RedirectView.as_view(url='/static/favicon.ico')),
     url(r'^api/', include(router.urls)),
     url(r'^api/token-auth/', 'rest_framework_jwt.views.obtain_jwt_token'),
     url(r'^api/conf/', ConfViewSet.as_view({'get': 'get'}))
