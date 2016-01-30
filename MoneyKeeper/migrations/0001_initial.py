cb787a595d75d01904f2af6aa8141f0d0b2b4fe9
ilov3 <iloveudead@gmail.com>
2016-01-30 19:19:46 +0300

    * migrations fixed

--- MoneyKeeper/migrations/0001_initial.py
+++ MoneyKeeper/migrations/0001_initial.py
@@ -41,9 +41,9 @@ class Migration(migrations.Migration):
                 ('amount', models.DecimalField(max_digits=12, decimal_places=2)),
                 ('comment', models.TextField(blank=True)),
                 ('kind', models.CharField(max_length=100, choices=[(b'exp', b'Expense'), (b'inc', b'Income'), (b'trn', b'Transfer')])),
-                ('account', models.ForeignKey(related_name='transactions', to='MoneyKeeper.models.account.Account')),
-                ('category', models.ForeignKey(related_name='transactions', blank=True, to='MoneyKeeper.models.category.Category', null=True)),
-                ('transfer_to_account', models.ForeignKey(related_name='transfers', blank=True, to='MoneyKeeper.models.account.Account', null=True)),
+                ('account', models.ForeignKey(related_name='transactions', to='MoneyKeeper.Account')),
+                ('category', models.ForeignKey(related_name='transactions', blank=True, to='MoneyKeeper.Category', null=True)),
+                ('transfer_to_account', models.ForeignKey(related_name='transfers', blank=True, to='MoneyKeeper.Account', null=True)),
                 ('user', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
             ],
             options={
