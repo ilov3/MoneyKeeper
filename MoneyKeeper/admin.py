8c445d6dec5e40fb37f27af9e6c50865fa2a94f0
ilov3 <iloveudead@gmail.com>
2016-02-05 17:02:55 +0300

    * admin page added for user profiles

--- MoneyKeeper/admin.py
+++ MoneyKeeper/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from models import Transaction, Account, Category
+from models.profile import UserProfile
 
 
 @admin.register(Transaction)
@@ -15,3 +16,8 @@ class AccountAdmin(admin.ModelAdmin):
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('user', 'name', 'kind', 'get_transactions_amount')
+
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'key_expires')
