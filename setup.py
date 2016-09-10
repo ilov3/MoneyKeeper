c2a807a04d13c36358e1c1f31070c2937ea593a4
ildarin22 <ildarimp@list.ru>
2016-09-11 01:02:56 +0300

    working!!!

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='MoneyKeeper',
+    version='',
+    packages=['MoneyKeeper', 'MoneyKeeper.utils', 'MoneyKeeper.views', 'MoneyKeeper.models', 'MoneyKeeper.migrations',
+              'MoneyKeeper.serializers', 'moneykeeper'],
+    url='',
+    license='',
+    author='ildar',
+    author_email='',
+    description=''
+)
